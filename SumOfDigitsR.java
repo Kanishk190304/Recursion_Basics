c50e0b67e1d53efbf8c714b1c8e2dad6f2386eaa
Kanishk Kaushik <143490272+Kanishk190304@users.noreply.github.com>
2024-09-27 21:38:26 +0530

    Add files via upload

---
+++ SumOfDigitsR.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class SumOfDigitsR {
+    
+    public static void main(String[] args) {
+        System.out.print("Enter any number and get the sum of it's digits : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Input
+        System.out.println("Sum of digits of " + n + " = " + findSum(n, 0)); // Output
+
+    }
+
+    public static int findSum(int n, int sum){
+        if(n==0){
+            return sum; // Base case
+        }
+        sum += n%10;
+        return findSum(n/10, sum); // Recursive call
+    }
+}
