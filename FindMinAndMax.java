62af187790255eac7cf6f96470755bf535b6b408
Kanishk Kaushik <143490272+Kanishk190304@users.noreply.github.com>
2024-10-05 14:45:04 +0530

    Add files via upload

---
+++ FindMinAndMax.java
@@ -0,0 +1,20 @@
+public class FindMinAndMax {
+    
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,2,3,8,4};
+        minAndmax(arr, arr[0], arr[0], 0);
+    }
+
+    public static void minAndmax(int arr[], int min, int max, int i){
+        if(i >= arr.length){
+            System.out.println("Min = "+ min +" Max = "+ max);
+        }
+        if(arr[i] < min){
+            min = arr[i];
+        }
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        minAndmax(arr, min, max, i+1);
+    }
+}
