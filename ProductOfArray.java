dcbf012f16a1db06817dba7f464dc29a5236d01a
Kanishk Kaushik <143490272+Kanishk190304@users.noreply.github.com>
2024-10-06 11:09:11 +0530

    Add files via upload

---
+++ ProductOfArray.java
@@ -0,0 +1,15 @@
+public class ProductOfArray {
+    
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(productOfArray(arr, 1, 0));
+    }
+
+    public static int productOfArray(int[] arr, int product, int i){
+        if(i >= arr.length){
+            return product;
+        }
+        product *= arr[i];
+        return productOfArray(arr, product, i+1);
+    }
+}
