6c6d5810a097ec441409a675cd550cef03cc1637
Kanishk Kaushik <143490272+Kanishk190304@users.noreply.github.com>
2024-10-08 17:28:22 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,15 @@
+public class ReverseNumber {
+    
+    public static void main(String[] args) {
+        int n = 123;
+        System.out.println(reverseNum(n, 0));
+    }
+
+    public static int reverseNum(int n, int rev){
+        if(n == 0){
+            return rev;
+        }
+        rev = rev * 10 + n%10;
+        return reverseNum(n/10, rev);
+    }
+}
