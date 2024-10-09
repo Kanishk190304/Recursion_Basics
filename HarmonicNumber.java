e9fb3ab13424aca9ea815d3fa7dc6f4da20b5d54
Kanishk Kaushik <143490272+Kanishk190304@users.noreply.github.com>
2024-10-09 18:07:18 +0530

    Add files via upload

---
+++ HarmonicNumber.java
@@ -0,0 +1,14 @@
+public class HarmonicNumber {
+    
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(harmonicNum(n));
+    }
+
+    public static double harmonicNum(int n){
+        if(n==1){
+            return 1.0;
+        }
+        return 1.0/n + harmonicNum(n-1);
+    }
+}
