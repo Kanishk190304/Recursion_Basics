1c1393f77198cd2e57f3ac8a0c529df12a67e418
Kanishk Kaushik <143490272+Kanishk190304@users.noreply.github.com>
2024-10-21 16:32:09 +0530

    Update PrintNumbers.java

--- PrintNumbers.java
+++ PrintNumbers.java
@@ -2,23 +2,23 @@ public class PrintNumbers {
     
     public static void main(String[] args) {
         int n = 5;
-        printNumbersNto1(n);
+        printNumbersNto1(n); 
         System.out.println();
         printNumbers1toN(n);
     }
 
     public static void printNumbersNto1(int n){
-        if(n==0){
+        if(n==0){ //base case
             return;
         }
         System.out.print(n + " ");
-        printNumbersNto1(n-1);
+        printNumbersNto1(n-1); //recursive call
     }
     public static void printNumbers1toN(int n){
         if(n==0){
             return;
         }
-        printNumbers1toN(n-1);
+        printNumbers1toN(n-1); //recursive call
         System.out.print(n + " ");
     }
 }
