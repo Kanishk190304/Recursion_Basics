da2dd6b4ca09704949089a6104b5b2f7a36ff767
Kanishk Kaushik <143490272+Kanishk190304@users.noreply.github.com>
2024-10-24 16:39:02 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -6,10 +6,10 @@ public class ReverseNumber {
     }
 
     public static int reverseNum(int n, int rev){
-        if(n == 0){
+        if(n == 0){ //base case
             return rev;
         }
         rev = rev * 10 + n%10;
-        return reverseNum(n/10, rev);
+        return reverseNum(n/10, rev); //recursive call
     }
 }
