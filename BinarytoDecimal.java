579c04f79618987f4e72d1c62eeabed3b262ba72
Kanishk Kaushik <143490272+Kanishk190304@users.noreply.github.com>
2024-10-26 22:10:06 +0530

    Update BinarytoDecimal.java

--- BinarytoDecimal.java
+++ BinarytoDecimal.java
@@ -6,10 +6,10 @@ public class BinarytoDecimal {
     }
 
     public static int bintodec(int bin, int dec, int i){
-        if(bin == 0){
+        if(bin == 0){ //base case
             return dec;
         }
         dec = dec + (bin % 10) * (int) Math.pow(2, i);
-        return bintodec(bin/10, dec, i+1);
+        return bintodec(bin/10, dec, i+1); //recursive call
     }
 }
